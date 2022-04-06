95a9c5b86aa06fd24f275e2838f84b48ec16b298
Keegan Kelly <keegankelly1@gmail.com>
2022-04-05 22:03:18 -0500

    API call added to Search component. Data is received from the endpoint.

--- src/public/Components/Search.js
+++ src/public/Components/Search.js
@@ -7,9 +7,21 @@ const Search = () => {
     requestGameDeals();
   }, []);
 
+  // async function requestPets() {
+  //   const res = await fetch(
+  //     `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+  //   );
+  //   const json = await res.json();
+  //   console.log(json);
+  //   setPets(json.pets);
+  // }
+
   async function requestGameDeals() {
-    const res = searchInput;
-    console.log(res);
+    const res = await fetch(
+      `https://www.cheapshark.com/api/1.0/games?title=${searchInput}&limit=60&exact=0`
+    );
+    const json = await res.json();
+    console.log(json);
   }
 
   return (
