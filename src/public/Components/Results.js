9970b3c22eb4c70ff194c30de5cc3030a284c547
Keegan Kelly <keegankelly1@gmail.com>
2022-04-28 21:51:33 -0500

    Fixed a nesting issue in the results table.

--- src/public/Components/Results.js
+++ src/public/Components/Results.js
@@ -7,28 +7,28 @@ const Results = ({games, searchInputLength}) => {
         else {
             return (
                 <div className="results">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Deal</th>
-                            </tr>
-                        </thead>
-                        <tbody>
                         {!games.length ? 
                             <h3>Sorry, we couldn&apos;t find anything that matched your search.</h3>
                             : 
-                            games.map((game) => {
-                                return (
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Deal</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {games.map((game) => {
+                                    return (
                                     <tr key={game.gameID}>
                                         <td>{game.external}</td>
                                         <td>{game.cheapest}</td>
                                     </tr>
-                                )    
-                            })
+                                    )    
+                                })}
+                                </tbody>
+                            </table>
                         }
-                        </tbody>
-                    </table>
                 </div>       
             )
         }
