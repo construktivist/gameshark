b55d70e383a32cfaa81ab14ce3423aa43f32ab43
Keegan Kelly <keegankelly1@gmail.com>
2022-05-23 22:20:40 -0500

    Results render

--- src/public/Components/Results.js
+++ src/public/Components/Results.js
@@ -1,44 +1,35 @@
 import React from "react";
 import {useState} from "react";
 
-const Results = ({games, searchInputLength}) => {
+const Results = ({games}) => {
 
-        const [plus, togglePlus] = useState("+")    
-
-        if (games.length < 1 && searchInputLength < 1) {
-            return (
-                <div className="results"></div>
-            )
-        }
-        else {
-            return (
-                <div className="results">
-                        {!games.length ? 
-                            <h3>Sorry, we couldn&apos;t find anything that matched your search.</h3>
-                            : 
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Deal</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                {games.map((game) => {
-                                    return (
-                                    <tr key={game.gameID}>
-                                        <td>{game.external}</td>
-                                        <td>{game.cheapest}</td>
-                                        <td><span onClick={e => { togglePlus("-")}}>{plus}</span></td>
-                                    </tr>
-                                    )    
-                                })}
-                                </tbody>
-                            </table>
-                        }
-                </div>       
-            )
-        }
+    const [plus, togglePlus] = useState('+')  
+        if (games.length < 1) {
+            return null;
+        }  
+        return (
+            <div className="results">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Deal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {games.map((game) => {
+                        return (
+                        <tr key={game.gameID}>
+                            <td>{game.external}</td>
+                            <td>{game.cheapest}</td>
+                            <td><span onClick={e => {plus === '+'? togglePlus("-") : togglePlus("+")}}>{plus}</span></td>
+                        </tr>
+                        )    
+                    })}
+                    </tbody>
+                </table>
+            </div>       
+        )
 }
 
 export default Results;
