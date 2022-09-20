1c8850f2efc8c5dd5b235116dbe0ceeaee18221c
Keegan Kelly <keegankelly1@gmail.com>
2022-09-19 21:42:12 -0500

    Added dealID and link to product page.

--- src/Components/TopStoreDeals.js
+++ src/Components/TopStoreDeals.js
@@ -1,10 +1,19 @@
 const TopStoreDeals = ({ game }) => {
+  const { title, normalPrice, dealID, salePrice } = game;
   return (
     <tr>
-      <td>{game.title}</td>
-      <td>{game.normalPrice}</td>
       <td>
-        <button>{game.salePrice}</button>
+        <button>More info.</button>
+      </td>
+      <td>{title}</td>
+      <td>{normalPrice}</td>
+      <td>
+        <a
+          rel="noreferrer"
+          href={`https://www.cheapshark.com/redirect?dealID=${dealID}`}
+        >
+          {salePrice}
+        </a>
       </td>
     </tr>
   );
